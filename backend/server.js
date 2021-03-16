14dd14589591bd6385ad904b2abc7d9a5ce59884
sbayrak <34121748+sbayrak@users.noreply.github.com>
2021-03-16 14:04:25 +0300

    Implemented custom error handling in backend

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
 import connectDB from './config/db.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import productRoutes from './routes/productRoutes.js';
 
 const app = express();
@@ -13,6 +14,9 @@ app.get('/', (req, res) => {
 });
 
 app.use('/api/products', productRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
