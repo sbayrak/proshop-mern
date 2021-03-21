9674ad277c955e736748f6c3440c60a26fd8b372
sbayrak <34121748+sbayrak@users.noreply.github.com>
2021-03-21 17:36:38 +0300

    Implemented register a user functionality

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -31,6 +31,41 @@ const authUser = asyncHandler(async (req, res) => {
   }
 });
 
+// @desc    REGISTER A NEW USER
+// @route   POST /api/users/
+// @access  Public
+const registerUser = asyncHandler(async (req, res) => {
+  let { name, email, password } = req.body;
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error('User already exists');
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  password = await bcrypt.hash(password, salt);
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error('Invalid user data');
+  }
+});
+
 // @desc    Get user profile
 // @route   GET /api/users/profile
 // @access  Private
@@ -49,4 +84,4 @@ const getUserProfile = asyncHandler(async (req, res) => {
     throw new Error('user not found');
   }
 });
-export { authUser, getUserProfile };
+export { authUser, registerUser, getUserProfile };
