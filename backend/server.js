db883ce87851d0e09881ede9587c5d560a2ed980
sbayrak <34121748+sbayrak@users.noreply.github.com>
2021-03-21 12:20:39 +0300

    Added authUser get token and return user methods

--- backend/server.js
+++ backend/server.js
@@ -4,16 +4,19 @@ dotenv.config();
 import connectDB from './config/db.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 
 const app = express();
 
 connectDB();
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('API is running');
 });
 
 app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
