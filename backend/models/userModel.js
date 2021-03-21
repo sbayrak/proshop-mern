ebff130e283401427cb4acb532c568237cefcd0a
sbayrak <34121748+sbayrak@users.noreply.github.com>
2021-03-21 17:35:39 +0300

    Removed matchPassword middleware from model

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -27,10 +27,6 @@ const userSchema = mongoose.Schema(
   }
 );
 
-userSchema.methods.matchPassword = async function (enteredPassword) {
-  return await bcrypt.compare(enteredPassword, this.password);
-};
-
 const User = mongoose.model('User', userSchema);
 
 export default User;
