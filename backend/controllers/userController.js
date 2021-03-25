b96882bc0be6a430d7e0d723a944b42a1e6bb24f
sbayrak <34121748+sbayrak@users.noreply.github.com>
2021-03-25 11:15:55 +0300

    Added updateUserProfile and checked with Postman

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -85,4 +85,33 @@ const getUserProfile = asyncHandler(async (req, res) => {
     throw new Error('user not found');
   }
 });
-export { authUser, registerUser, getUserProfile };
+
+// @desc    Update user profile
+// @route   PUT /api/users/profile
+// @access  Private
+const updateUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user.id);
+
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    if (req.body.password) {
+      const salt = await bcrypt.genSalt(10);
+      let password = await bcrypt.hash(req.body.password, salt);
+      user.password = password;
+    }
+
+    const updatedUser = await user.save();
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+      token: generateToken(updatedUser._id),
+    });
+  } else {
+    res.status(404);
+    throw new Error('user not found');
+  }
+});
+export { authUser, registerUser, getUserProfile, updateUserProfile };
