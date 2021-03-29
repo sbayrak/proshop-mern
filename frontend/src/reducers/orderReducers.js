d44b52ec268efbffcf77ae1ec4713831e8a26583
sbayrak <34121748+sbayrak@users.noreply.github.com>
2021-03-29 11:22:40 +0300

    Implemented createOrder action and reducer

---
+++ frontend/src/reducers/orderReducers.js
@@ -0,0 +1,30 @@
+import {
+  ORDER_CREATE_REQUEST,
+  ORDER_CREATE_SUCCESS,
+  ORDER_CREATE_FAIL,
+} from '../constants/orderContants';
+
+export const createOrderReducer = (state = {}, action) => {
+  const { type, payload } = action;
+
+  switch (type) {
+    case ORDER_CREATE_REQUEST:
+      return {
+        loading: true,
+      };
+
+    case ORDER_CREATE_SUCCESS:
+      return {
+        loading: false,
+        success: true,
+        order: payload,
+      };
+    case ORDER_CREATE_FAIL:
+      return {
+        loading: false,
+        error: payload,
+      };
+    default:
+      return state;
+  }
+};
