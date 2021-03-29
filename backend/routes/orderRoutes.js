19ba27e7134aa048630568ca66eeefc10f20f780
sbayrak <34121748+sbayrak@users.noreply.github.com>
2021-03-29 12:18:09 +0300

    Added getOrderById controler along with name email

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,8 +1,9 @@
 import protect from '../middleware/authMiddleware.js';
-import { addOrderItems } from '../controllers/orderController.js';
+import { addOrderItems, getOrderById } from '../controllers/orderController.js';
 import express from 'express';
 const router = express.Router();
 
 router.route('/').post(protect, addOrderItems);
 
+router.route('/:id').get(protect, getOrderById);
 export default router;
