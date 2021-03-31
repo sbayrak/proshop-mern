34c3b4d6396c401aa4a02b478775ff41fc4dc8d0
sbayrak <34121748+sbayrak@users.noreply.github.com>
2021-03-31 11:57:19 +0300

    Added function for getting logged in users orders

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -3,11 +3,13 @@ import {
   addOrderItems,
   getOrderById,
   updateOrderToPaid,
+  getMyOrders,
 } from '../controllers/orderController.js';
 import express from 'express';
 const router = express.Router();
 
 router.route('/').post(protect, addOrderItems);
+router.route('/').get(protect, getMyOrders);
 
 router.route('/:id').get(protect, getOrderById);
 router.route('/:id/pay').put(protect, updateOrderToPaid);
