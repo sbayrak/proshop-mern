77d5876a8e8d374bf411f16e4f21a7cef7062302
sbayrak <34121748+sbayrak@users.noreply.github.com>
2021-04-03 12:15:19 +0300

    Added create and update product controllers

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -4,11 +4,18 @@ import {
   getProductById,
   getProducts,
   deleteProduct,
+  updateProduct,
+  createProduct,
 } from '../controllers/productController.js';
 import { protect, admin } from '../middleware/authMiddleware.js';
 
 router.route('/').get(getProducts);
 
-router.route('/:id').get(getProductById).delete(protect, admin, deleteProduct);
+router
+  .route('/:id')
+  .get(getProductById)
+  .delete(protect, admin, deleteProduct)
+  .put(protect, admin, updateProduct)
+  .post(protect, admin, createProduct);
 
 export default router;
